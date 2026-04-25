5ee37290fd232b8e3fa5864a567ba8390020e4dc
Raúl Antuña Suárez <rolitoansu@outlook.com>
2026-04-25 23:05:36 +0200

    First commit

---
+++ BeanChat/src/main/java/com/raul/beanchat/BeanChatApplication.java
@@ -0,0 +1,13 @@
+package com.raul.beanchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanChatApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BeanChatApplication.class, args);
+    }
+
+}
---
+++ BeanChat/src/test/java/com/raul/beanchat/BeanChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raul.beanchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
